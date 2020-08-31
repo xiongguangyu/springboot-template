ff4d0dc701bc3d8dcf5fff1bfec4a3bb63c0bdf7
xiongguangyu <xq9466474664@163.com>
2020-08-31 15:15:52 +0800

    ...

--- src/test/java/com/example/order/OrderApplicationTests.java
+++ src/test/java/com/example/order/OrderApplicationTests.java
@@ -12,7 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -48,7 +50,7 @@ class OrderApplicationTests {
     public void test1(){
         Long userId = 5L;
         List<GSysMenu> menuList = userService.getMenuList(userId);
-        System.out.println();
+        System.out.println(menuList);
     }
 
 
