39db72b4a4d6232b4b5a47dbf3ae2adb07ddcf68
admin <xq9466474664@163.com>
2020-09-22 16:16:26 +0800

    代码提交

--- src/main/java/com/example/order/controller/ManageController.java
+++ src/main/java/com/example/order/controller/ManageController.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,7 +20,7 @@ import java.util.Map;
 
 /**
  * @ClassName ManageController
- * @Description 公告,新闻，轮播图Controller
+ * @Description 小程序端首页公告,新闻，轮播图Controller
  * @Author xionggy
  * @Date 2020/9/10
  * @Version 1.0
@@ -35,31 +34,12 @@ public class ManageController {
     @Autowired
     ManageService manageService;
 
-
-    @RequestMapping(value = "/getInfo",method = RequestMethod.GET)
-    public void getList(@RequestParam("objId") Long objId,
-                            @RequestParam("type") String type,
-                            HttpServletRequest request, HttpServletResponse response){
-        Map<String, Object> res = new HashMap<String, Object>();
-        try {
-            GSysManage gSysManage= manageService.getInfo(objId,type);
-            res.put(Constant.RESPONSE_CODE, Constant.SUCCEED_CODE_VALUE);
-            res.put(Constant.RESPONSE_DATA, gSysManage);
-            ServletUtils.writeToResponse(response, res);
-        } catch (LoginException e) {
-            res.put(Constant.RESPONSE_CODE, Constant.FAIL_CODE_VALUE);
-            res.put(Constant.RESPONSE_CODE_MSG, e.getMessage());
-            ServletUtils.writeToResponse(response, res);
-        }
-
-    }
-
     @RequestMapping(value = "/getList",method = RequestMethod.GET)
     public void getInfo(@RequestParam("type") String type,@RequestParam(value="searchContent",required=false)String searchContent,
-                            HttpServletRequest request, HttpServletResponse response){
+                        HttpServletRequest request, HttpServletResponse response){
         Map<String, Object> res = new HashMap<String, Object>();
         try {
-            List<GSysManage> gSysManageList= manageService.getList(type,searchContent);
+            List<GSysManage> gSysManageList= manageService.getManageList(type,searchContent);
             res.put(Constant.RESPONSE_CODE, Constant.SUCCEED_CODE_VALUE);
             res.put(Constant.RESPONSE_DATA, gSysManageList);
             ServletUtils.writeToResponse(response, res);
@@ -68,8 +48,23 @@ public class ManageController {
             res.put(Constant.RESPONSE_CODE_MSG, e.getMessage());
             ServletUtils.writeToResponse(response, res);
         }
-
     }
 
+    @RequestMapping(value = "/getInfo",method = RequestMethod.GET)
+    public void getList(@RequestParam("objId") Long objId,
+                            @RequestParam("type") String type,
+                            HttpServletRequest request, HttpServletResponse response){
+        Map<String, Object> res = new HashMap<String, Object>();
+        try {
+            GSysManage gSysManage= manageService.getManageInfo(objId,type);
+            res.put(Constant.RESPONSE_CODE, Constant.SUCCEED_CODE_VALUE);
+            res.put(Constant.RESPONSE_DATA, gSysManage);
+            ServletUtils.writeToResponse(response, res);
+        } catch (LoginException e) {
+            res.put(Constant.RESPONSE_CODE, Constant.FAIL_CODE_VALUE);
+            res.put(Constant.RESPONSE_CODE_MSG, e.getMessage());
+            ServletUtils.writeToResponse(response, res);
+        }
+    }
 
 }
